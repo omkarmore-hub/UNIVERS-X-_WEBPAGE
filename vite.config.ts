43c8017bb5b3d24089817e5e1f705b2a5d402f23
Omkar More <omkarharidasmore@gmail.com>
2026-07-28 06:49:35 +0530

    bug name 1.4

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base : '/UNIVERS(X)_WEBPAGE/',
+    base : '/UNIVERS-X-_WEBPAGE/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
